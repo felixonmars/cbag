091580980fe06f8da230f4d60a4983789a52d3f8
Eric Chang <pkerichang@gmail.com>
2018-07-21 07:56:35 +0200

    1. working on netlister.

---
+++ include/cbag/netlist.h
@@ -0,0 +1,94 @@
+/** \file netlist.h
+ *  \brief This file defines classes that netlists objects.
+ *
+ *  \author Eric Chang
+ *  \date   2018/07/10
+ */
+
+#ifndef CBAG_NETLIST_H
+#define CBAG_NETLIST_H
+
+#include <string>
+#include <vector>
+#include <map>
+#include <fstream>
+
+#include <cbag/spirit/ast.h>
+
+
+namespace cbag {
+
+    spirit::ast::name_unit parse_cdba_name_unit(const std::string &source);
+
+    spirit::ast::name parse_cdba_name(const std::string &source);
+
+    spirit::ast::name parse_cdba_name(const char *source);
+
+    std::string to_string_cdba(const spirit::ast::name_bit &bit);
+
+    struct SchCellView;
+    struct PinFigure;
+
+    using term_t = std::map<std::string, PinFigure>;
+
+    class NetlistBuilder {
+    public:
+        class LineBuilder {
+        public:
+            LineBuilder(size_t ncol, char cnt_char, bool break_before)
+                    : tokens(), ncol(ncol), cnt_char(cnt_char), break_before(break_before) {}
+
+            friend LineBuilder &operator<<(LineBuilder &builder, const std::string &token);
+
+            friend LineBuilder &operator<<(LineBuilder &builder, std::string &&token);
+
+            friend std::ofstream &operator<<(std::ofstream &stream, const LineBuilder &b);
+
+        private:
+            std::vector<std::string> tokens;
+            size_t ncol;
+            char cnt_char;
+            bool break_before;
+        };
+
+        explicit NetlistBuilder(const char *fname);
+
+        void build();
+
+        void add_cellview(const std::string &name, const SchCellView &cv);
+
+    protected:
+        std::ofstream out_file;
+    private:
+        virtual void write_header() = 0;
+
+        virtual void write_end() = 0;
+
+        virtual void write_cv_header(const std::string &name, const term_t &in_terms,
+                                     const term_t &out_terms, const term_t &io_terms) = 0;
+
+        virtual void write_cv_end(const std::string &name) = 0;
+    };
+
+    class SpiceBuilder : NetlistBuilder {
+    public:
+        explicit SpiceBuilder(const char *fname) : NetlistBuilder(fname) {}
+
+    private:
+        void write_header() override;
+
+        void write_end() override;
+
+        void write_cv_header(const std::string &name, const term_t &in_terms,
+                             const term_t &out_terms, const term_t &io_terms) override;
+
+        void write_cv_end(const std::string &name) override;
+
+        static const size_t ncol = 80;
+        static const char cnt_char = '+';
+        static const bool break_before = false;
+    };
+}
+
+
+#endif //CBAG_NETLIST_H
--- src/cbag/cbag.cpp
+++ src/cbag/cbag.cpp
@@ -11,9 +11,6 @@
 #include <yaml-cpp/yaml.h>
 #include <easylogging++.h>
 
-#include <cbag/spirit/parsers.h>
-#include <cbag/spirit/name_unit.h>
-#include <cbag/spirit/name.h>
 #include <cbag/database/yaml_cellviews.h>
 
 INITIALIZE_EASYLOGGINGPP // NOLINT
@@ -29,29 +26,5 @@ namespace cbag {
         emitter << node;
         outfile << emitter.c_str() << std::endl;
         outfile.close();
-    }
-
-    spirit::ast::name_unit parse_cdba_name_unit(const std::string &source) {
-        spirit::ast::name_unit ast;
-        parse(source.c_str(), source.size(), spirit::name_unit(), ast);
-        return ast;
-    }
-
-    spirit::ast::name_unit parse_cdba_name_unit(const char *source) {
-        spirit::ast::name_unit ast;
-        parse(source, strlen(source), spirit::name_unit(), ast);
-        return ast;
-    }
-
-    spirit::ast::name parse_cdba_name(const std::string &source) {
-        spirit::ast::name ast;
-        parse(source.c_str(), source.size(), spirit::name(), ast);
-        return ast;
-    }
-
-    spirit::ast::name parse_cdba_name(const char *source) {
-        spirit::ast::name ast;
-        parse(source, strlen(source), spirit::name(), ast);
-        return ast;
     }
 }
