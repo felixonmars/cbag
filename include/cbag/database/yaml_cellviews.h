146eaff15f2af3de8e6b63e7b9d2a3ea68858e9c
Eric Chang <pkerichang@gmail.com>
2018-07-31 06:05:26 -0700

    1. Added YAML conversion methods for unordered_map
    2. remove yaml_cellviews.cpp file.  Project now compiles.

--- include/cbag/database/yaml_cellviews.h
+++ include/cbag/database/yaml_cellviews.h
@@ -13,4 +13,33 @@
 #include <cbag/database/yaml_datatypes.h>
 #include <cbag/database/yaml_figures.h>
 
+// YAML conversion methods for std::unordered_map
+namespace YAML {
+template <typename K, typename V> struct convert<std::unordered_map<K, V>> {
+    static Node encode(const std::unordered_map<K, V> &rhs) {
+        Node node(NodeType::Map);
+        for (auto const &x : rhs) {
+            node.force_insert(x.first, x.second);
+        }
+        return node;
+    }
+
+    static bool decode(const Node &node, std::unordered_map<K, V> &rhs) {
+        if (!node.IsMap())
+            return false;
+
+        rhs.clear();
+        for (const_iterator it = node.begin(); it != node.end(); ++it)
+#if defined(__GNUC__) && __GNUC__ < 4
+            // workaround for GCC 3:
+            rhs[it->first.template as<K>()] = it->second.template as<V>();
+#else
+            rhs[it->first.as<K>()] = it->second.as<V>();
+#endif
+        return true;
+    }
+};
+
+} // namespace YAML
+
 #endif // CBAG_DATABASE_YAML_CELLVIEWS_H
