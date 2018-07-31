8b4a05ea8ffbabe4248eb91d9a32ab93522dd1bf
Eric Chang <pkerichang@gmail.com>
2018-07-31 09:49:42 -0700

    adding file transfer script.

---
+++ transfer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+scp -r include erichang@bwrcrdsl-6.eecs.berkeley.edu:~/projects/cbag/
+scp -r src erichang@bwrcrdsl-6.eecs.berkeley.edu:~/projects/cbag/
+scp -r test erichang@bwrcrdsl-6.eecs.berkeley.edu:~/projects/cbag/
+scp CMakeLists.txt erichang@bwrcrdsl-6.eecs.berkeley.edu:~/projects/cbag/
+scp build/install.sh erichang@bwrcrdsl-6.eecs.berkeley.edu:~/projects/cbag/build/
