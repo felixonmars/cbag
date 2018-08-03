213e989f365a612875f67826db2cef90ab65e8f5
Eric Chang <pkerichang@gmail.com>
2018-08-03 07:41:14 -0700

    1. added netmap_sample test executable 2. remove compile_commands.json from repo, as different machines may have different flags.

---
+++ test/netmap_sample.cpp
@@ -0,0 +1,25 @@
+#include <cbag/database/yaml_cellviews.h>
+#include <yaml-cpp/yaml.h>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    cbag::SchCellViewInfo info;
+    cbag::lib_map_t lib_map;
+    cbag::netlist_map_t net_map;
+
+    info.io_terms.push_back("D");
+    info.io_terms.push_back("G");
+    info.io_terms.push_back("B");
+    info.io_terms.push_back("S");
+    info.props.emplace("nfin", std::string("4"));
+    info.props.emplace("l", std::string("16n"));
+    info.props.emplace("nf", std::string("2"));
+
+    lib_map.emplace("nch", info);
+    net_map.emplace("cds_ff_mpt", lib_map);
+
+    YAML::Emitter emitter;
+    emitter << YAML::Node(net_map);
+
+    std::cout << emitter.c_str() << std::endl;
+}
