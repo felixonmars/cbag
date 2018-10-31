84710dc6e9657664b3cd0a97ec6d394b2ce6e9e5
Eric Chang <pkerichang@gmail.com>
2018-10-30 21:46:59 -0700

    rename build_test.sh to run_test.sh.

--- run_test.sh
+++ run_test.sh
@@ -6,3 +6,4 @@ export BUILD_TYPE=${1:-Debug}
 echo "CMAKE_BULD_TYPE: ${BUILD_TYPE}"
 cmake -H. -B_build -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
 cmake --build _build --target test_main -- -j 8
+./_build/test/test_main
