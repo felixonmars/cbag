462e0ace8cfc5a9a4ff3e0916f32bdb1db90f960
Eric Chang <pkerichang@gmail.com>
2018-10-30 21:49:25 -0700

    update run_test script.

--- run_test.sh
+++ run_test.sh
@@ -4,6 +4,6 @@ source .bashrc
 
 export BUILD_TYPE=${1:-Debug}
 echo "CMAKE_BULD_TYPE: ${BUILD_TYPE}"
-cmake -H. -B_build -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
-cmake --build _build --target test_main -- -j 8
+cmake -H. -B_build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} || exit 1
+cmake --build _build --target test_main -- -j 8 || exit 1
 ./_build/test/test_main
