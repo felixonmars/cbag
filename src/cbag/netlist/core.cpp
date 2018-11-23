c4fce3f4b8a2f49428ac8c76d20e437b29035440
Eric Chang <pkerichang@gmail.com>
2018-11-23 08:16:38 -0800

    Make sure to flush output file when closing.

--- src/cbag/netlist/core.cpp
+++ src/cbag/netlist/core.cpp
@@ -7,7 +7,10 @@ namespace netlist {
 
 nstream_file::nstream_file(const std::string &fname) : out_file(util::open_file_write(fname)) {}
 
-void nstream_file::close() { out_file.close(); }
+void nstream_file::close() {
+    out_file.flush();
+    out_file.close();
+}
 
 } // namespace netlist
 } // namespace cbag
