a09618d20e42a931d1066c9a7cee82fbcd8b4b70
Eric Chang <pkerichang@gmail.com>
2018-12-11 10:27:06 -0800

    update run_test script to use ccache.

--- run_test.sh
+++ run_test.sh
@@ -8,6 +8,6 @@ fi
 
 export BUILD_TYPE=${1:-Debug}
 echo "CMAKE_BULD_TYPE: ${BUILD_TYPE}"
-cmake -H. -B_build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} || exit 1
+cmake -H. -B_build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_CXX_COMPILER_LAUNCHER=ccache || exit 1
 cmake --build _build --target test_main -- -j 8 || exit 1
 ./_build/tests/test_main
