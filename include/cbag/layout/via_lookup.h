cdb21ecfb6ff0a1dff85bcebf10c97609fc442a2
Eric Chang <pkerichang@gmail.com>
2018-12-30 23:13:50 +0800

    Update tech_params.yaml format, working on via code.

--- include/cbag/layout/via_lookup.h
+++ include/cbag/layout/via_lookup.h
@@ -24,14 +24,12 @@ using lay_map_t = std::unordered_map<std::string, lay_t>;
 using purp_map_t = std::unordered_map<std::string, purp_t>;
 using via_lay_purp_t = std::tuple<layer_t, layer_t, layer_t>;
 using vlp_map_t = std::unordered_map<std::string, via_lay_purp_t>;
-using vname_map_t = std::unordered_map<int_t, std::string>;
 using vlayers_t = std::pair<layer_t, layer_t>;
 using vid_map_t = std::unordered_map<vlayers_t, std::string, boost::hash<vlayers_t>>;
 using vinfo_map_t = std::unordered_map<std::string, std::vector<via_info>>;
 
 class via_lookup {
   private:
-    vname_map_t name_map;
     vlp_map_t lp_map;
     vid_map_t id_map;
     vinfo_map_t info_map;
--- src/cbag/layout/via_lookup.cpp
+++ src/cbag/layout/via_lookup.cpp
@@ -11,34 +11,50 @@
 namespace cbag {
 namespace layout {
 
-lay_t get_layer_id(const lay_map_t &lay_map, const std::string &layer) {
+lay_t layer_id_at(const lay_map_t &lay_map, const std::string &layer) {
     auto iter = lay_map.find(layer);
     if (iter == lay_map.end())
         throw std::out_of_range("Cannot find layer ID for layer: " + layer);
     return iter->second;
 }
 
-purp_t get_purpose_id(const purp_map_t &purp_map, const std::string &purpose) {
+purp_t purpose_id_at(const purp_map_t &purp_map, const std::string &purpose) {
     auto iter = purp_map.find(purpose);
     if (iter == purp_map.end())
         throw std::out_of_range("Cannot find purpose ID for purpose: " + purpose);
     return iter->second;
 }
 
+const std::string &via_id_at(const vid_map_t &id_map, layer_t bot_lay, layer_t top_lay) {
+    auto iter = id_map.find(std::make_pair(std::move(bot_lay), std::move(top_lay)));
+    if (iter == id_map.end())
+        throw std::out_of_range(fmt::format("Cannot find via ID between ({}, {}) and ({}, {})",
+                                            bot_lay.first, bot_lay.second, top_lay.first,
+                                            top_lay.second));
+    return iter->second;
+}
+
+const std::vector<via_info> &via_info_at(const vinfo_map_t &info_map, const std::string &via_id) {
+    auto iter = info_map.find(via_id);
+    if (iter == info_map.end())
+        throw std::out_of_range("Cannot find via infor for via ID: " + via_id);
+
+    return iter->second;
+}
+
 vlayers_t parse_via_layers(const YAML::Node &node, const lay_map_t &lay_map,
                            const purp_map_t &purp_map) {
-    return {layer_t{get_layer_id(lay_map, node[0][0].as<std::string>()),
-                    get_purpose_id(purp_map, node[0][1].as<std::string>())},
-            layer_t{get_layer_id(lay_map, node[1][0].as<std::string>()),
-                    get_purpose_id(purp_map, node[1][1].as<std::string>())}};
+    return {layer_t{layer_id_at(lay_map, node[0][0].as<std::string>()),
+                    purpose_id_at(purp_map, node[0][1].as<std::string>())},
+            layer_t{layer_id_at(lay_map, node[1][0].as<std::string>()),
+                    purpose_id_at(purp_map, node[1][1].as<std::string>())}};
 }
 
 via_lookup::via_lookup() = default;
 
 via_lookup::via_lookup(const YAML::Node &parent, const lay_map_t &lay_map,
                        const purp_map_t &purp_map)
-    : name_map(parent["via_name"].as<vname_map_t>()), lp_map(parent["via_layers"].as<vlp_map_t>()),
-      info_map(parent["via"].as<vinfo_map_t>()) {
+    : lp_map(parent["via_layers"].as<vlp_map_t>()), info_map(parent["via"].as<vinfo_map_t>()) {
     for (const auto &node : parent["via_id"]) {
         id_map.emplace(parse_via_layers(node.first, lay_map, purp_map),
                        node.second.as<std::string>());
@@ -55,6 +71,9 @@ via_lay_purp_t via_lookup::get_via_layer_purpose(const std::string &key) const {
 
 via_param via_lookup::get_via_params(const box_t &box, layer_t bot_layer, layer_t top_layer,
                                      orient_2d bot_dir, orient_2d top_dir, bool extend) const {
+    auto via_id = via_id_at(id_map, std::move(bot_layer), std::move(top_layer));
+    auto vinfo_list = via_info_at(info_map, via_id);
+
     return {};
 }
 
